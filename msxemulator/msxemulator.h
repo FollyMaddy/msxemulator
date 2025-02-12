6d28f62b1b5377799c4d4efa5c640c75db39f5db
Mitsuru Aoki <shippoiincho@gmail.com>
2025-02-12 11:41:15 +0900

    fix config parameter

--- msxemulator/msxemulator.h
+++ msxemulator/msxemulator.h
@@ -1,4 +1,4 @@
 // Configuration
-#define USE_I2S     // Enable I2S DAC Output and SCC emulation
-#define USE_OPLL    // Enable OPLL emulation. need FM-PAC BIOS.
+//#define USE_I2S     // Enable I2S DAC Output and SCC emulation
+//#define USE_OPLL    // Enable OPLL emulation. need FM-PAC BIOS.
 #define USE_FDC     // Enable SONY HBD-F1 emulation. need DISKBIOS.
